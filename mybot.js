ce6f35cc9938f8b1d8c991a026fa3fa4f7919923
Kristofer Hamnell <kristofer.hamnell@ericsson.com>
2019-01-03 12:56:42 +0100

    initial commit

--- mybot.js
+++ mybot.js
@@ -1,16 +1,17 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
- 
-client.on("message", (message) => {
-    if (message.content.startsWith("/kick")) {
-        var member= message.mentions.members.first();
-        member.kick().then((member) => {
-            message.channel.send(":wave: " + member.displayName + " has been successfully kicked :point_right: ");
-        }).catch(() => {
-            message.channel.send("Access Denied");
-        });
-    }
-});
- 
-client.login("***");
+const Discord = require('discord.js');
+const { token } = require('./config.json');
 
+const client = new Discord.Client();
+
+client.on('message', (message) => {
+  if (message.content.startsWith('/kick')) {
+    const member = message.mentions.members.first();
+    member.kick().then((member) => {
+      message.channel.send(`:wave: ${member.displayName} has been successfully kicked :point_right: `);
+    }).catch(() => {
+      message.channel.send('Access Denied');
+    });
+  }
+});
+
+client.login('***');
