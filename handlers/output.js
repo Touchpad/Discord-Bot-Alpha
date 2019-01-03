4f2d784836176f885afd077d1d80412a3bd59fd6
Kristofer Hamnell <kristofer.hamnell@ericsson.com>
2019-01-03 23:40:54 +0100

    Update name of project to a generic one for now

--- handlers/output.js
+++ handlers/output.js
@@ -1,7 +1,21 @@
+/**
+ * Output handler
+ *
+ * This file contains overrides for console-output to add a bit of styling including timestamp to the output
+ * To use this file just require it in the root file of your project
+ */
+
 const chalk = require('chalk');
+const { logError } = require('./error');
 
 const _log = console.log;
 
+/**
+ * This function creates a timestamp: YYYY-MM-DD HH:mm:SS
+ * the return value is formatted for cli and slightly dimmed
+ *
+ * @returns string
+ */
 const timestamp = () => {
   const _date = new Date();
   const date = [
@@ -18,18 +32,51 @@ const timestamp = () => {
   return chalk.dim(`${date.join('-')} ${time.join(':')}`);
 };
 
+/**
+ * Override for console.log
+ * adds timestamp to output
+ *
+ * @param message
+ */
 console.log = (...message) => {
   _log.apply(console, [timestamp(), chalk.blue.bold(' ->'.padStart(8, ' ')), ...message]);
 };
 
+/**
+ * Override for console.info
+ * adds timestamp and " Info  ->" in magenta to output
+ *
+ * @param message
+ */
 console.info = (...message) => {
   _log.apply(console, [timestamp(), chalk.magenta('Info  ->'), ...message]);
 };
 
+/**
+ * Override for console.warn
+ * adds timestamp and " Warn  ->" in yellow to output
+ *
+ * @param message
+ */
 console.warn = (...message) => {
   _log.apply(console, [timestamp(), chalk.yellow('Warn  ->'), ...message]);
 };
 
-console.error = (...message) => {
+/**
+ * Override for console.error
+ * adds timestamp and " Error ->" in red to output
+ *
+ * @param msg mixed
+ */
+console.error = (...msg) => {
+  const { NODE_ENV } = process.env;
+
+  const message = (msg[0].constructor === Object) ? msg[0].message : msg;
+
   _log.apply(console, [timestamp(), chalk.red('Error ->'), ...message]);
+
+  if (NODE_ENV === 'production') {
+    const raw = (msg[0].constructor === Object) ? msg[0].stack : msg;
+    logError(...raw);
+  }
 };
