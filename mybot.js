ba0842a4ce5c1f30e87078f7f9c444e526d88d39
Kristofer Hamnell <kristofer.hamnell@ericsson.com>
2019-01-04 01:14:05 +0100

    Reverting root-file for now

--- mybot.js
+++ mybot.js
@@ -1,17 +1,16 @@
-const Discord = require('discord.js');
-const { token, prefix } = require('./config.json');
-
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.on('message', (message) => {
-  if (message.content.startsWith(`${prefix}kick`)) {
-    const member = message.mentions.members.first();
+client.on("message", (message) => {
+  if (message.content.startsWith("/kick")) {
+    var member= message.mentions.members.first();
     member.kick().then((member) => {
-      message.channel.send(`:wave: ${member.displayName} has been successfully kicked :point_right: `);
+      message.channel.send(":wave: " + member.displayName + " has been successfully kicked :point_right: ");
     }).catch(() => {
-      message.channel.send('Access Denied');
+      message.channel.send("Access Denied");
     });
   }
 });
 
-client.login(token);
+client.login("***");
+
